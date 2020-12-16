357095b7de2931db811065295b0827f19e5be722
grvgarg22 <gaurav.garg22@outlook.com>
2020-12-16 10:06:22 +0530

    update 0.0.1

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,21 @@
+$('#myCarousel').carousel({
+    interval: 1000
+})
 
 $('.contentCarousel .carousel-item').each(function() {
-	var minPerSlide = 3;
-	var next = $(this).next();
-	if (!next.length) {
-		next = $(this).siblings(':first');
-	}
-	next.children(':first-child').clone().appendTo($(this));
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-	for (var i = 0; i < minPerSlide; i++) {
-		next = next.next();
-		if (!next.length) {
-			next = $(this).siblings(':first');
-		}
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
 
-		next.children(':first-child').clone().appendTo($(this));
-	}
+        next.children(':first-child').clone().appendTo($(this));
+    }
 });
